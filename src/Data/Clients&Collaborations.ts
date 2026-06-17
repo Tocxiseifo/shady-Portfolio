aeda301f4b07d9355e409629f96b715392044d0f
TocxiSeifo <seifogamer391@gmail.com>
2026-06-17 05:04:02 +0300

    #

--- src/Data/Clients&Collaborations.ts
+++ src/Data/Clients&Collaborations.ts
@@ -4,70 +4,70 @@ export const clientsAndCollaborations: clientCollaboration[] = [
     {
         id:0,
         name: "3li Boltx",
-        image: "/public/assets/img/722406978_4484645441805045_679404346637260438_n.jpg",
+        image: "/src/assets/img/722406978_4484645441805045_679404346637260438_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@3liboltx"
     },
     {
         id:1,
         name: "Abdo2xd",
-        image: "/public/assets/img/721994975_1037265658982989_8914435243804504296_n.jpg",
+        image: "/src/assets/img/721994975_1037265658982989_8914435243804504296_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@Abdo2xd/videos"
     },
     {
         id:2,
         name: "Amr Beshr - عمرو بشر",
-        image: "/public/assets/img/719899609_1311013074440031_1768603241444350199_n.jpg",
+        image: "/src/assets/img/719899609_1311013074440031_1768603241444350199_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@amrbeshr/shorts"
     },
     {
         id:3,
         name: "LeOniDes - مروان ريحان",
-        image: "/public/assets/img/722489491_1523295416094988_6359129478923679169_n.jpg",
+        image: "/src/assets/img/722489491_1523295416094988_6359129478923679169_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@marwanrehan/videos"
     },
     {
         id:4,
         name: "B3shr",
-        image: "/public/assets/img/723585591_2481355395670371_7140576204021438481_n.jpg",
+        image: "/src/assets/img/723585591_2481355395670371_7140576204021438481_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@B3shr"
     },
     {
         id:5,
         name: "B3shrextraa",
-        image: "/public/assets/img/721816854_1554343602916873_6912848651086614021_n.jpg",
+        image: "/src/assets/img/721816854_1554343602916873_6912848651086614021_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@B3shrextraa"
     },
     {
         id:6,
         name: "LiveBoltx",
-        image: "/public/assets/img/720873621_2837333366618340_3975298025645269691_n.jpg",
+        image: "/src/assets/img/720873621_2837333366618340_3975298025645269691_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@LiveBoltx"
     },
     {
         id:7,
         name: "BoltxShorts",
-        image: "/public/assets/img/719216212_1350676007200553_1481312602846631226_n.jpg",
+        image: "/src/assets/img/719216212_1350676007200553_1481312602846631226_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@BoltxShorts"
     },
     {
         id:8,
         name: "Sheshtawy - ششتا",
-        image: "/public/assets/img/724275443_2666910607036162_6541566201500534309_n.jpg",
+        image: "/src/assets/img/724275443_2666910607036162_6541566201500534309_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@sheshtawyx/videos"
     },
     {
         id:9,
         name: "Aywa - ايوه",
-        image: "/public/assets/img/721265616_1355412726654688_787119595811157606_n.jpg",
+        image: "/src/assets/img/721265616_1355412726654688_787119595811157606_n.jpg",
         subs: "100K",
         link: "https://www.youtube.com/@aywa_10/videos"
     },
--- src/Data/PortfolioData.ts
+++ src/Data/PortfolioData.ts
@@ -3,109 +3,109 @@ import type { PortfolioTypes } from "../types/PortfolioTypes";
 export const PortfolioData : PortfolioTypes[] =[
     {
         id:0,
-        img:'/public/assets/portfolio_img/724292590_905621625883797_611440256394363083_n.jpg',
+        img:'/src/assets/portfolio_img/724292590_905621625883797_611440256394363083_n.jpg',
         link:"https://youtu.be/DASeULB40RU?si=oh3pjYo39i8MHkDA",
         title:"جمعت 10 يوتيوبرز في Among Us 3D ( الجزء 2 )"
     },
     {
         id:1,
-        img:'/public/assets/portfolio_img/721332496_2154573838653934_8814948065972649804_n.jpg',
+        img:'/src/assets/portfolio_img/721332496_2154573838653934_8814948065972649804_n.jpg',
         link:"https://youtu.be/0i7RPg0kWj4?si=601R4s7BuXB-crDG",
         title:"جمعت 10 يوتيوبرز في Among Us 3D"
     },
     {
         id:2,
-        img:'/public/assets/portfolio_img/721189240_1454949073316676_6304371492218070253_n.jpg',
+        img:'/src/assets/portfolio_img/721189240_1454949073316676_6304371492218070253_n.jpg',
         link:"https://youtu.be/tj5MvodQ9rE?si=qRDp73hcPcFCCukP",
         title:"رسميا بولتكس في برشلونة | الطريق الى الكرة الذهبية"
     },
     {
         id:3,
-        img:'/public/assets/portfolio_img/724305748_982601644744034_8924422338105247568_n.jpg',
+        img:'/src/assets/portfolio_img/724305748_982601644744034_8924422338105247568_n.jpg',
         link:"https://youtu.be/29LiVoxvvkI?si=_HswIV-TmnFMhshw",
         title:"رسميا بولتكس خارج نوتنجهام | الطريق الى الكرة الذهبية"
     },
     {
         id:4,
-        img:'/public/assets/portfolio_img/720552434_1329601565970688_2070597340717822343_n.jpg',
+        img:'/src/assets/portfolio_img/720552434_1329601565970688_2070597340717822343_n.jpg',
         link:"https://youtu.be/YIJeFefZhjk?si=8poM8bnJPMPTUuvv",
         title:"الهروب من الباك رومز مع اليوتيوبرز"
     },
     {
         id:5,
-        img:'/public/assets/portfolio_img/720919225_1540455080926199_5306596573126704222_n.jpg',
+        img:'/src/assets/portfolio_img/720919225_1540455080926199_5306596573126704222_n.jpg',
         link:"https://youtu.be/oVtbsr82YFA?si=i64pSPnbwzmboS6e",
         title:"جمعت اليوتيوبرز في لعبة مافيا"
     },
     {
         id:6,
-        img:'/public/assets/portfolio_img/721733550_3318737331620805_907594532305321243_n.jpg',
+        img:'/src/assets/portfolio_img/721733550_3318737331620805_907594532305321243_n.jpg',
         link:"https://youtu.be/fhLDCeDjwW0?si=a09QiWzWaI534Q7Z",
         title:"بولتكس و مروان في المقبرة الفرعونية"
     },
     {
         id:7,
-        img:'/public/assets/portfolio_img/722702717_1489776522990691_4083203031055010773_n.jpg',
+        img:'/src/assets/portfolio_img/722702717_1489776522990691_4083203031055010773_n.jpg',
         link:"https://youtu.be/AhV9asPaspc?si=l38M2labKb30qL-8",
         title:"الناس عشوائيين هيحددو تشكيلتي"
     },
     {
         id:8,
-        img:'/public/assets/portfolio_img/720751034_887896184353681_7276585771940883814_n.jpg',
+        img:'/src/assets/portfolio_img/720751034_887896184353681_7276585771940883814_n.jpg',
         link:"https://youtu.be/u-FRpb1UhXo?si=-GQR6vV0ppRV-s6s",
         title:"ختمت كل رياضات العالم"
     },
     {
         id:9,
-        img:'/public/assets/portfolio_img/598658914_880911057784252_821029753749398538_n.jpg',
+        img:'/src/assets/portfolio_img/598658914_880911057784252_821029753749398538_n.jpg',
         link:"https://youtu.be/wBXv53kwIhA?si=jcRV1UWO2kQrlIhL",
         title:"انا و بولتكس ملزوقين في بعض غصب عننا !! 😭 هو النص الي تحت وانا النص الي فوق"
     },
     {
         id:10,
-        img:'/public/assets/portfolio_img/722993337_4403987913163200_1230082691068335606_n.jpg',
+        img:'/src/assets/portfolio_img/722993337_4403987913163200_1230082691068335606_n.jpg',
         link:"https://youtu.be/houUFh1mHQs?si=jclKe3rEruZ6hSnJ",
         title:"أكتر لعبة مستفزة في العالم !! 🤬 لو خسرت هتتهكر واللعبة تختفي للأبد"
     },
     {
         id:11,
-        img:'/public/assets/portfolio_img/724113576_1324986572458158_5492234562607825485_n.jpg',
+        img:'/src/assets/portfolio_img/724113576_1324986572458158_5492234562607825485_n.jpg',
         link:"https://youtu.be/0MjRg2eaItA?si=29Tbfa7vS-sWUGim",
         title:"باكرومز مع اليوتيوبرز"
     },
     {
         id:12,
-        img:'/public/assets/portfolio_img/721134729_2123491601545118_7338846137250592830_n.jpg',
+        img:'/src/assets/portfolio_img/721134729_2123491601545118_7338846137250592830_n.jpg',
         link:"https://youtu.be/ZA4jhEPRGno?si=yfWO2pG2oRBzo0f4",
         title:"ريماتش  مع اليوتيوبرز"
     },
     {
         id:13,
-        img:'/public/assets/portfolio_img/719648160_1000819626270500_4321652931831423344_n.jpg',
+        img:'/src/assets/portfolio_img/719648160_1000819626270500_4321652931831423344_n.jpg',
         link:"https://www.youtube.com/watch?v=e1toMf-g5z0&t=1060s",
         title:'تحدي أوصل لأوفر ١١٦ في حدث راجناروك 😱 FC Mobile'
     },
     {
         id:14,
-        img:'/public/assets/portfolio_img/723125941_2136789597179119_8306410091749804799_n.jpg',
+        img:'/src/assets/portfolio_img/723125941_2136789597179119_8306410091749804799_n.jpg',
         link:"https://www.youtube.com/watch?v=WpuYF1m01C4&t=33s",
         title:'بشر و كريم يلعب في ريماتش 🎮 REMATCH'
     },
     {
         id:15,
-        img:'/public/assets/portfolio_img/722752378_996798639943078_4629258629492314403_n.jpg',
+        img:'/src/assets/portfolio_img/722752378_996798639943078_4629258629492314403_n.jpg',
         link:"https://youtu.be/u-Bq2hoee9g?si=R-9ZQ_rJdqS1sL2c",
         title:'أحسن بداية في FC موبايل مع عبدو 🤩🔥'
     },
     {
         id:16,
-        img:'/public/assets/portfolio_img/722162066_4569918626572062_7188726707224374302_n.jpg',
+        img:'/src/assets/portfolio_img/722162066_4569918626572062_7188726707224374302_n.jpg',
         link:"https://youtu.be/RvBUEoaIL_o?si=AR_I7tjG91KlTrkv",
         title:'فوازير أيوا مع عبدو 2XD و بولتكس 😂⏲️'
     },
     {
         id:17,
-        img:'/public/assets/portfolio_img/722223709_4548067368806307_1614385261560342093_n.jpg',
+        img:'/src/assets/portfolio_img/722223709_4548067368806307_1614385261560342093_n.jpg',
         link:"https://youtu.be/WVF2HhXD60o?si=WuFFDpaPPNNW0hxD",
         title:'اول مشاركة ليا في كأس العالم | الطريق للكرة الذهبية'
     },
